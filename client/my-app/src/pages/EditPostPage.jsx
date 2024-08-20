7edde92ba3b015b957ab9db594f4bf783969a2a2
yosef <yosef@ASUS-X507U>
2024-08-20 22:23:24 +0300

    screen

--- client/my-app/src/pages/EditPostPage.jsx
+++ client/my-app/src/pages/EditPostPage.jsx
@@ -53,7 +53,7 @@ export const EditPostPage = () => {
             onSubmit={(e) => e.preventDefault()}
         >
             <label className='text-gray-300 py-2 bg-gray-600 text-xs mt-2 flex items-center justify-center border-2 border-dotted cursor-pointer'>
-                Прикрепить изорбажение:
+                Attach image:
                 <input
                     type='file'
                     className='hidden'
